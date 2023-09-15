54b668ea27ec7165b2db0b8f4b0f84b4eb93fc38
Felix Burkard <felix-burkard@web.de>
2023-09-16 00:15:23 +0200

    Added Breadth-first algorithm for evaluating NFAs

--- automata.hs
+++ automata.hs
@@ -1,10 +1,8 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Eta reduce" #-}
-{-# HLINT ignore "Use map" #-}
-
 data DFA state input = DFA state (state -> input -> state) (state -> Bool)
 
 data NFA state input = NFA state (state -> input -> [state]) (state -> Bool) input
+
+data Result = Success | Failure | Continue deriving (Show)
 
 evalDFA :: DFA a b -> [b] -> Bool
 evalDFA (DFA init trans acc) input = acc . foldr (flip trans) init $ input
@@ -12,21 +10,32 @@ evalDFA (DFA init trans acc) input = acc . foldr (flip trans) init $ input
 generateConfigs :: (state -> input -> [state]) -> state -> input -> [input] -> [(state, [input])]
 generateConfigs transition state input rest = zip (transition state input) (repeat rest)
 
+genConfigs :: (state -> input -> [state]) -> input -> (state, [input]) -> [(state, [input])]
+genConfigs transition epsilon config = configs <> epsilonConfigs
+  where configs 
+          | null . snd $ config = []
+          | otherwise = generateConfigs transition (fst config) (head . snd $ config) (tail . snd $ config)
+        epsilonConfigs = generateConfigs transition (fst config) epsilon (snd config)
+
+evaluateConfigs :: (state -> Bool) -> [(state, [input])] -> Result
+evaluateConfigs _ [] = Failure
+evaluateConfigs accept configs
+  | any (accept . fst) . filter (null . snd) $ configs = Success
+  | otherwise = Continue
+
 evalNFA :: NFA a b -> [b] -> Bool
 evalNFA (NFA initial transition accept epsilon) vals = run (initial, vals)
   where generate = generateConfigs transition
         run (state, []) = accept state || any run (generate state epsilon [])
         run (state, x:xs) = any run (generate state x xs <> generate state epsilon (x:xs))
-        
-data State = Odd | Even deriving (Show, Eq)
-data Input = Zero | One deriving (Show)
 
-transFunc :: State -> Input -> State
-transFunc Odd Zero = Odd
-transFunc Odd One = Even
-transFunc Even Zero = Even
-transFunc Even One = Odd
+evalNFABreadthFirst :: NFA a b -> [b] -> Bool
+evalNFABreadthFirst (NFA initial transition accept epsilon) vals = run [(initial, vals)]
+  where run vals = case evaluateConfigs accept vals of
+                        Success -> True
+                        Failure -> False
+                        Continue -> run . concatMap (genConfigs transition epsilon) $ vals
 
-testDFA = DFA Even transFunc (Even ==)
 
-test = evalDFA testDFA
+
+
