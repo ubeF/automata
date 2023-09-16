150c48c7d32ea6e6a6a93d122e34d5cc0fce8331
Felix Burkard <felix-burkard@web.de>
2023-09-16 17:01:17 +0200

    Added Config Type

--- src/Automata/NFA.hs
+++ src/Automata/NFA.hs
@@ -7,29 +7,33 @@ data NFA state input = NFA state (state -> input -> [state]) (state -> Bool)
 
 data Result = Success | Failure | Continue deriving (Show)
 
+data Config state input = Config state [input]
+
 eval :: Alphabet b => NFA a b -> [b] -> Bool
-eval (NFA initial transition accept) vals = run [(initial, vals)]
+eval (NFA initial transition accept) vals = run [Config initial vals]
   where run vals = case evaluateConfigs accept vals of
                         Success -> True
                         Failure -> False
-                        Continue -> run . concatMap (genConfigs transition) $ vals
+                        Continue -> run . concatMap (generateConfigs transition) $ vals
 
-generateConfigs :: (state -> input -> [state]) -> state -> input -> [input] -> [(state, [input])]
-generateConfigs transition state input rest = zip (transition state input) (repeat rest)
+generateConfigs :: Alphabet a => (b -> a -> [b]) -> Config b a -> [Config b a]
+generateConfigs transition (Config state word) = configs <> epsilonConfigs
+  where gen input rest = zipWith Config (transition state input) (repeat rest)
+        configs 
+          | null word = []
+          | otherwise = gen (head word) (tail word)
+        epsilonConfigs = gen epsilon word
 
-genConfigs :: Alphabet input => (state -> input -> [state]) -> (state, [input]) -> [(state, [input])]
-genConfigs transition config = configs <> epsilonConfigs
-  where configs 
-          | null . snd $ config = []
-          | otherwise = generateConfigs transition (fst config) (head . snd $ config) (tail . snd $ config)
-        epsilonConfigs = generateConfigs transition (fst config) epsilon (snd config)
+evaluateConfig :: (a -> Bool) -> Config a b -> Bool
+evaluateConfig accept (Config state word) 
+  | null word = accept state
+  | otherwise = False 
 
-evaluateConfigs :: (state -> Bool) -> [(state, [input])] -> Result
+evaluateConfigs :: (a -> Bool) -> [Config a b] -> Result
 evaluateConfigs _ [] = Failure
 evaluateConfigs accept configs
-  | any (accept . fst) . filter (null . snd) $ configs = Success
+  | any (evaluateConfig accept) configs = Success
   | otherwise = Continue
-
 
 -- evalNFA :: NFA a b -> [b] -> Bool
 -- evalNFA (NFA initial transition accept epsilon) vals = run (initial, vals)
