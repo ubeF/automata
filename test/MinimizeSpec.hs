9eb70d10483dab387b6264f0b778a88852765ea1
Felix Burkard <felix-burkard@web.de>
2023-09-27 00:49:01 +0200

    Added Property test for minimization

--- test/MinimizeSpec.hs
+++ test/MinimizeSpec.hs
@@ -1,7 +1,23 @@
 module MinimizeSpec (spec) where
 
 import Test.Hspec
+import Test.QuickCheck
 import Automata.DFA
+
+newtype Word = Word String deriving (Show)
+
+cons :: Char -> MinimizeSpec.Word -> MinimizeSpec.Word
+cons x (Word xs) = Word (x:xs)
+
+instance Arbitrary MinimizeSpec.Word where
+  arbitrary = sized $ \n ->
+    frequency
+      [ (1, return (MinimizeSpec.Word []))
+      , (n, cons <$> elements "ab" <*> arbitrary)
+      ]
+
+prop_sameResult :: DFA Int Char -> MinimizeSpec.Word -> Bool
+prop_sameResult dfa (Word word) = eval dfa word == eval (minimize dfa) word
 
 transitions :: [(Int, Char, Int)]
 transitions = [
@@ -20,14 +36,8 @@ transitions = [
 dfa :: DFA Int Char
 dfa = DFA 0 transitions [3,4] "ab"
 
-minimized :: DFA Int Char
-minimized = minimize dfa
-
 spec :: Spec
 spec = do
-  describe "Normal DFA" $ do
-    it "It recognizes 'ab'" $ do
-      eval dfa "ab" `shouldBe` True
-  describe "Minimized DFA" $ do
-    it "It recognizes 'ab'" $ do
-      eval minimized "ab" `shouldBe` True
+  describe "Minimization of DFAs" $ do
+    it "Recognizes the same words" $ 
+      property (prop_sameResult dfa) 
